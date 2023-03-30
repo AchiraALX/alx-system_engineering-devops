eb037996f64906ddce6de760a0f79ccce41ab74e
Jacob Achira <111626469+AchiraALX@users.noreply.github.com>
2023-03-30 03:14:47 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,41 @@
-# Installs a Nginx server
-
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+# Install Nginx package
+package { 'nginx':
+  ensure => 'installed',
 }
+
+# Configure Nginx
+file { '/etc/nginx/sites-available/default':
+  content => "
+    server {
+        listen 80;
+        root /var/www/html;
+        index index.html;
+        location / {
+            # Return "Hello World!" on GET request to root
+            if (\$request_method = GET) {
+                return 200 'Hello World!';
+            }
+        }
+        location /redirect_me {
+            # Perform 301 redirect
+            return 301 /;
+        }
+    }
+  ",
+  notify  => Service['nginx'],
+}
+
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Start and enable Nginx service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
