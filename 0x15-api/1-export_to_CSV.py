e4e98ead1c7146404199b08f28de991e668bf780
AchiraALX <ed127720@students.mu.ac.ke>
2023-04-25 04:07:38 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,8 @@
 """
 This is a Python script that retrieves employee data from a mock REST API
 and exports the data in CSV format. The script uses the urllib module
-to make HTTP requests to the API and the sys module to retrieve command-line arguments.
+to make HTTP requests to the API and the sys module to retrieve command-line
+arguments.
 """
 
 import csv
@@ -38,7 +39,18 @@ if __name__ == "__main__":
                 })
 
     with open(f"{user_id}.csv", "w+", newline="") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
+        fieldnames = [
+            "USER_ID",
+            "USERNAME",
+            "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"
+        ]
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames=fieldnames,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_ALL
+        )
         writer.writeheader()
         writer.writerows(all_tasks)
