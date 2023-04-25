51a55151ecf8724e3ded9a78f26f0f507f44aaf6
AchiraALX <ed127720@students.mu.ac.ke>
2023-04-25 04:20:56 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
 """
-This is a Python script that retrieves employee data from a mock REST API
-and exports the data in CSV format. The script uses the urllib module
-to make HTTP requests to the API and the sys module to retrieve command-line
-arguments.
+Extend the python script from exercise 0 to export data in CSV format.
+Record all tasks that are owned by the employee.
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
 """
-
 import csv
-import json
-import urllib.request
+import requests
 import sys
 
-
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = f"http://jsonplaceholder.typicode.com/users/{user_id}"
-    with urllib.request.urlopen(url) as response:
-        username = json.loads(response.read().decode())["username"]
+    eid = sys.argv[1]
+    username = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(eid)).json().get("username")
     all_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
-    # Structure of all_tasks list:
-    # [
-    #   [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE],
-    #   [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE],
-    #   ...
-    # ]
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            temp = []
+            temp.extend((eid,
+                         username,
+                         task.get("completed"),
+                         task.get("title")))
+            all_tasks.append(temp)
 
-    url = "http://jsonplaceholder.typicode.com/todos"
-    with urllib.request.urlopen(url) as response:
-        tasks = json.loads(response.read().decode())
-        for task in tasks:
-            if task.get("userId") == int(user_id):
-                all_tasks.append({
-                    "USER_ID": user_id,
-                    "USERNAME": username,
-                    "TASK_COMPLETED_STATUS": task.get("completed"),
-                    "TASK_TITLE": task.get("title")
-                })
-
-    with open(f"{user_id}.csv", "w+", newline="") as csvfile:
-        fieldnames = [
-            "USER_ID",
-            "USERNAME",
-            "TASK_COMPLETED_STATUS",
-            "TASK_TITLE"
-        ]
-        writer = csv.DictWriter(
-            csvfile,
-            fieldnames=fieldnames,
-            delimiter=",",
-            quotechar='"',
-            quoting=csv.QUOTE_ALL
-        )
-        writer.writeheader()
+    with open("{}.csv".format(eid), 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerows(all_tasks)
