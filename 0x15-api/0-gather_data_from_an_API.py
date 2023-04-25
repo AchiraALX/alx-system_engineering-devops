029d144863e52d18153c990131800a1d13fe2db6
AchiraALX <ed127720@students.mu.ac.ke>
2023-04-25 04:01:05 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
 This is a Python script that retrieves employee data from a mock REST API
-and outputs the number of tasks completed by the employee, as well as the titles
-of the completed tasks. The script uses urllib to make HTTP requests to the API
-and the sys module to retrieve command-line arguments.
+and outputs the number of tasks completed by the employee, as well as the
+titles of the completed tasks. The script uses urllib to make HTTP requests
+to the API and the sys module to retrieve command-line arguments.
 """
 
 import sys
@@ -16,14 +16,18 @@ if __name__ == "__main__":
     eid = sys.argv[1]
 
     # Retrieve employee name from API
-    with urllib.request.urlopen("http://jsonplaceholder.typicode.com/users/{}".format(eid)) as response:
+    with urllib.request.urlopen(
+        "http://jsonplaceholder.typicode.com/users/{}".format(eid)
+    ) as response:
         data = json.loads(response.read().decode())
         name = data.get("name")
 
     # Count completed tasks for employee
     total_tasks = 0
     done_tasks = []
-    with urllib.request.urlopen("http://jsonplaceholder.typicode.com/todos") as response:
+    with urllib.request.urlopen(
+        "http://jsonplaceholder.typicode.com/todos"
+    ) as response:
         data = json.loads(response.read().decode())
         for task in data:
             if task.get("userId") == int(eid):
@@ -32,6 +36,9 @@ if __name__ == "__main__":
                     done_tasks.append(task.get("title"))
 
     # Output results
-    print("Employee {} is done with tasks({}/{}):".format(name, len(done_tasks), total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(name, len(done_tasks),
+                                                        total_tasks)
+    )
     for item in done_tasks:
         print("\t{}".format(item))
