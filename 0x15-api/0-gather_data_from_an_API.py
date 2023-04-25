cd29815db143bb71fe4c64c1c6a24bf8832aec3c
AchiraALX <ed127720@students.mu.ac.ke>
2023-04-25 04:26:14 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,29 @@
 #!/usr/bin/python3
 """
-This is a Python script that retrieves employee data from a mock REST API
-and outputs the number of tasks completed by the employee, as well as the
-titles of the completed tasks. The script uses urllib to make HTTP requests
-to the API and the sys module to retrieve command-line arguments.
+Write a script that hits a REST api that has data on employees and tasks
+and filters the data based on an argument passed to the script.
+The argument is the employee ID, and the output should display the employee
+name and the tasks the employee completed.
 """
-
+import requests
 import sys
-import urllib.request
-import json
-
 
 if __name__ == "__main__":
-    # Get employee ID from command-line argument
     eid = sys.argv[1]
-
-    # Retrieve employee name from API
-    with urllib.request.urlopen(
-        "http://jsonplaceholder.typicode.com/users/{}".format(eid)
-    ) as response:
-        data = json.loads(response.read().decode())
-        name = data.get("name")
-
-    # Count completed tasks for employee
+    name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                        .format(eid)).json().get("name")
     total_tasks = 0
     done_tasks = []
-    with urllib.request.urlopen(
-        "http://jsonplaceholder.typicode.com/todos"
-    ) as response:
-        data = json.loads(response.read().decode())
-        for task in data:
-            if task.get("userId") == int(eid):
-                total_tasks += 1
-                if task.get("completed"):
-                    done_tasks.append(task.get("title"))
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
-    # Output results
-    print(
-        "Employee {} is done with tasks({}/{}):".format(name, len(done_tasks),
-                                                        total_tasks)
-    )
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            total_tasks += 1
+            if (task.get("completed")):
+                done_tasks.append(task.get("title"))
+
+    print("Employee {} is done with tasks({:d}/{:d}):"
+          .format(name, len(done_tasks), total_tasks))
+
     for item in done_tasks:
-        print("\t{}".format(item))
+        print("\t {}".format(item))
