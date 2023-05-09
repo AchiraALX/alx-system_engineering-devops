299bb869ebe40f731f03c6cea1ef63ae7fef855a
AchiraALX <ed127720@students.mu.ac.ke>
2023-05-09 22:28:53 +0300

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit
+    If the subreddit is invalid, return 0
+    """
+    # Make a request to the Reddit API to get the sureddit information
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers={"User-Agent": "Mozilla/5.0"},
+        allow_redirects=False
+    )
+
+    # Check response if it was successful and not  redirect
+    if response.status_code != 200 or response.is_redirect:
+        return 0
+
+    # Parse the JSO response to get the number of subscribers
+    try:
+        data = response.json()
+        return data["data"]["subscribers"]
+    except (KeyError, ValueError):
+        return 0
+
+print(number_of_subscribers("DragonflyAntique4675"))
